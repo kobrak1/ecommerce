c822567ffab99e7f99dd4aad023589bb733bfb30
kobrak1 <burakkarhan657@gmail.com>
2023-10-18 23:55:28 +0300

    new

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Policy from './components/Policy/Policy'
 import Sliders from './components/Sliders/Slider'
 import Categories from './components/Categories/Categories'
 import Products from './components/Products/Products'
+import './App.css'
 
 const App = () => {
   return (
