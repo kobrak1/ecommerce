b77629be693ddb89d5da4c977f421b6378a32216
kobrak1 <burakkarhan657@gmail.com>
2023-10-22 20:41:07 +0300

    CampaignSingle.jsx file is added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,15 @@
 import './App.css'
 //components
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Policy from './components/Policy/Policy'
-import Sliders from './components/Sliders/Slider'
-import Categories from './components/Categories/Categories'
-import Products from './components/Products/Products'
-import Campaigns from './components/Campaigns/Campaigns'
-import Blogs from './components/Blogs/Blogs'
-import Brands from './components/Brands/Brands'
+import HomePage from './pages/HomePage'
+import ShopPage from './pages/ShopPage'
+import ContactPage from './pages/ContactPage'
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <Sliders />
-      <Categories />
-      <Products />
-      <Campaigns />
-      <Blogs />
-      <Brands />
-      <Policy />
-      <Footer />
+      <HomePage />
+      <ShopPage />
+      <ContactPage />
     </div>
   )
 }
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,29 @@
+import Header from "../components/Header/Header"
+import Sliders from "../components/Sliders/Slider"
+import Categories from "../components/Categories/Categories"
+import Products from "../components/Products/Products"
+import Campaigns from "../components/Campaigns/Campaigns"
+import Blogs from "../components/Blogs/Blogs"
+import CampaignSingle from "../components/CampaignSingle/CampaignSingle"
+import Brands from "../components/Brands/Brands"
+import Policy from "../components/Policy/Policy"
+import Footer from "../components/Footer/Footer"
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <Sliders />
+      <Categories />
+      <Products />
+      <Campaigns />
+      <Blogs />
+      <CampaignSingle />
+      <Brands />
+      <Policy />
+      <Footer />
+    </>
+  )
+}
+
+export default HomePage
