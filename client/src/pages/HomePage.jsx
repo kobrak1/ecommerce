7717c76136996c897b29b2e7c1845b32cb746a01
kobrak1 <burakkarhan657@gmail.com>
2023-10-27 11:59:43 +0300

    Header and Footer moved to MainLayout.jsx from all the components

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,17 +1,16 @@
-import Header from "../components/Header/Header"
-import Sliders from "../components/Sliders/Slider"
-import Categories from "../components/Categories/Categories"
-import Products from "../components/Products/Products"
-import Campaigns from "../components/Campaigns/Campaigns"
-import Blogs from "../components/Blogs/Blogs"
-import CampaignSingle from "../components/CampaignSingle/CampaignSingle"
-import Brands from "../components/Brands/Brands"
-import Footer from "../components/Footer/Footer"
+import Header from "../components/Header/Header";
+import Sliders from "../components/Sliders/Slider";
+import Categories from "../components/Categories/Categories";
+import Products from "../components/Products/Products";
+import Campaigns from "../components/Campaigns/Campaigns";
+import Blogs from "../components/Blogs/Blogs";
+import CampaignSingle from "../components/CampaignSingle/CampaignSingle";
+import Brands from "../components/Brands/Brands";
+import Footer from "../components/Footer/Footer";
 
 const HomePage = () => {
   return (
     <>
-      <Header />
       <Sliders />
       <Categories />
       <Products />
@@ -19,9 +18,8 @@ const HomePage = () => {
       <Blogs />
       <CampaignSingle />
       <Brands />
-      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
