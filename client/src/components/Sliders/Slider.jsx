f820105be4febf836230a5c5fc2a2c3188040536
kobrak1 <burakkarhan657@gmail.com>
2023-10-28 17:17:16 +0300

    feat: added functions for Slider buttons and Slider dots

--- client/src/components/Sliders/Slider.jsx
+++ client/src/components/Sliders/Slider.jsx
@@ -1,35 +1,62 @@
-import './Slider.css'
-import SliderItem from './SliderItem'
+import { useState } from "react";
+import SliderItem from "./SliderItem";
+// css file
+import "./Slider.css";
 
 const Sliders = () => {
+  const [counter, setCounter] = useState(0);
+
+  // button functions
+  const decrease = () => setCounter(counter < 2 ? counter + 1 : 0);
+  const increase = () => setCounter(counter > 0 ? counter - 1 : 2);
+
   return (
     <>
-    <section className="slider">
+      <section className="slider">
         <div className="slider-elements">
-            <SliderItem />
-            <div className="slider-buttons">
-                <button onClick="plusSlide(-1)">
-                <i className="bi bi-chevron-left"></i>
-                </button>
-                <button onClick="plusSlide(1)">
-                <i className="bi bi-chevron-right"></i>
-                </button>
-            </div>
-            <div className="slider-dots">
-                <button className="slider-dot active" onClick="currentSlide(1)">
-                <span></span>
-                </button>
-                <button className="slider-dot" onClick="currentSlide(2)">
-                <span></span>
-                </button>
-                <button className="slider-dot" onClick="currentSlide(3)">
-                <span></span>
-                </button>
-            </div>
-        </div>
-    </section>
-    </>
-  )
-}
+          {counter === 0 && (
+            <SliderItem imageSrc="../../../public/img/slider/slider1.jpg" />
+          )}
 
-export default Sliders
+          {counter === 1 && (
+            <SliderItem imageSrc="../../../public/img/slider/slider2.jpg" />
+          )}
+
+          {counter === 2 && (
+            <SliderItem imageSrc="../../../public/img/slider/slider3.jpg" />
+          )}
+          <div className="slider-buttons">
+            <button onClick={() => increase()}>
+              <i className="bi bi-chevron-left"></i>
+            </button>
+            <button onClick={() => decrease()}>
+              <i className="bi bi-chevron-right"></i>
+            </button>
+          </div>
+          <div className="slider-dots">
+            <button
+              className={`slider-dot ${counter === 0 && "active"}`}
+              onClick={() => setCounter(0)}
+            >
+              <span></span>
+            </button>
+            <button
+              className={`slider-dot ${counter === 1 && "active"}`}
+              onClick={() => setCounter(1)}
+            >
+              <span></span>
+            </button>
+            <button
+              className={`slider-dot ${counter === 2 && "active"}`}
+              onClick={() => setCounter(2)}
+            >
+              <span></span>
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Sliders;
