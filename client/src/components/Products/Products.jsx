7c33f4f245ccb31b0764e37d6aed4c676934f58d
kobrak1 <burakkarhan657@gmail.com>
2023-10-29 11:37:05 +0300

    feat: autoplay feature is added to slider

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -39,6 +39,8 @@ const Products = () => {
     slidesToScroll: 1,
     nextArrow: <NextBtn />,
     prevArrow: <PrevBtn />,
+    autoplaySpeed: 3000,
+    autoplay: true,
     responsive: [
       {
         breakpoint: 992,
