73da2c53aca17309cb1ad638695136cc980ddd61
kobrak1 <burakkarhan657@gmail.com>
2023-10-29 21:22:45 +0300

    fix: react-slick slider slide button problems solved

--- client/src/components/ProducDetails/Gallery/Gallery.jsx
+++ client/src/components/ProducDetails/Gallery/Gallery.jsx
@@ -1,9 +1,53 @@
 import { useState } from "react";
 import productsData from "../../../data.json";
+import Slider from "react-slick";
+import PropTypes from "prop-types";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./Gallery.css";
 
+const NextBtn = ({ onClick }) => {
+  return (
+    <button
+      className="glide__arrow glide__arrow--right"
+      onClick={onClick}
+      style={{ zIndex: "5" }}
+    >
+      <i className="bi bi-chevron-right"></i>
+    </button>
+  );
+};
+NextBtn.propTypes = {
+  onClick: PropTypes.func,
+};
+
+const PrevBtn = ({ onClick }) => {
+  return (
+    <button
+      className="glide__arrow glide__arrow--left"
+      onClick={onClick}
+      style={{ zIndex: "5" }}
+    >
+      <i className="bi bi-chevron-left"></i>
+    </button>
+  );
+};
+PrevBtn.propTypes = {
+  onClick: PropTypes.func,
+};
+
 const Gallery = () => {
-  const [activeImage, setActiveImage] = useState(productsData[0].img.thumbs[0]);
+  const [activeImage, setActiveImage] = useState(
+    productsData[0].img.singleImage
+  );
+  let settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <NextBtn />,
+    prevArrow: <PrevBtn />,
+  };
 
   return (
     <div className="product-gallery">
@@ -12,27 +56,17 @@ const Gallery = () => {
       </div>
       <div className="product-thumb">
         <div className="glide__track" data-glide-el="track">
-          <ol className="gallery-thumbs glide__slides">
+          <Slider {...settings}>
             {productsData[0].img.thumbs.map((item, index) => (
-              <li className="glide__slide glide__slide--active" key={index} onClick={() => setActiveImage(item)}>
+              <li
+                className="glide__slide glide__slide--active"
+                key={index}
+                onClick={() => setActiveImage(item)}
+              >
                 <img src={item} alt="" className="img-fluid active" />
               </li>
             ))}
-          </ol>
-        </div>
-        <div className="glide__arrows" data-glide-el="controls">
-          <button
-            className="glide__arrow glide__arrow--left"
-            data-glide-dir="<"
-          >
-            <i className="bi bi-chevron-left"></i>
-          </button>
-          <button
-            className="glide__arrow glide__arrow--right"
-            data-glide-dir=">"
-          >
-            <i className="bi bi-chevron-right"></i>
-          </button>
+          </Slider>
         </div>
       </div>
     </div>
