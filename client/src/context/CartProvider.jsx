c815072926379e58cdfac16fa3925672a0230f73
kobrak1 <burakkarhan657@gmail.com>
2023-11-04 16:41:59 +0300

    fix: CartItems data saved to the localStorage

--- client/src/context/CartProvider.jsx
+++ client/src/context/CartProvider.jsx
@@ -1,14 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(
+    localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : []
+  );
   console.log("cart items:", cartItems);
   const addToCart = (item) => {
     setCartItems((prevCart) => [...prevCart, item]);
   };
+
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+
   return (
     <CartContext.Provider
       value={{
