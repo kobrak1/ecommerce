3aa666de5e7b63f487ec1d7df8a78b7976996044
kobrak1 <burakkarhan657@gmail.com>
2023-11-06 22:53:38 +0300

    fix: mongodb connected to the server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
-const monggose = require("mongoose");
+const { default: mongoose } = require("mongoose");
 const port = 5000;
 const app = express();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://kobrak1:altoBrawn1@e-commerce-z.z9n10dq.mongodb.net/"
+    );
+    console.log("Connected to MongoDB");
+  } catch (error) {}
+};
 
 app.get("/", (req, res) => {
   res.send("goodbye fucking world");
@@ -12,5 +21,6 @@ app.get("/getto", (req, res) => {
 });
 
 app.listen(port, () => {
+  connect();
   console.log(`Sunucu ${port} portunda calisiyor.`);
 });
