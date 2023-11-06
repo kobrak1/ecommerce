d0509d0e44b6a2a46ce4bec01df143e13098c965
kobrak1 <burakkarhan657@gmail.com>
2023-11-06 22:39:45 +0300

    add: express.js modules are installed

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
-const express = require("express")
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send('goodbye fucking world')
+})
+
+app.get("/getto", (req, res) => {
+    res.send('goodbye')
+})
+
+app.listen(5000, () => {
+    console.log(`Sunucu ${5000} portunda calisiyor.`);
+})
