c16914a845a185867c53d2dc8556e6b7fcaf3fb0
kobrak1 <burakkarhan657@gmail.com>
2023-11-06 16:18:53 +0300

    add: new function setCargoFee state is added. It allows users to set a new fast cargo price

--- client/src/components/Cart/CartTotals.jsx
+++ client/src/components/Cart/CartTotals.jsx
@@ -1,15 +1,23 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../../context/CartProvider";
 
 const CartTotals = () => {
+  const [fastCargoChecked, setFastCargoChecked] = useState(false);
+  const [cargoFee, setCargoFee] = useState(15);
   const { cartItems } = useContext(CartContext);
+  // create an array of prices of each items in the cart
   const cartItemTotals = cartItems.map((item) => {
     const itemTotal = item.price.newPrice * item.quantity;
     return itemTotal;
   });
+  // calculate subtotal price
   const subTotals = cartItemTotals.reduce((previousValue, currentValue) => {
     return previousValue + currentValue;
   }, 0);
+  // set fast cargo price
+  const fastCargoPrice = (fee) => setCargoFee(fee);
+  console.log(fastCargoPrice);
+
   return (
     <div className="cart-totals">
       <h2>Cart totals</h2>
@@ -27,8 +35,13 @@ const CartTotals = () => {
               <ul>
                 <li>
                   <label>
-                    Fast Cargo: $15.00
-                    <input type="checkbox" id="fast-cargo" />
+                    Fast Cargo: ${cargoFee}
+                    <input
+                      type="checkbox"
+                      id="fast-cargo"
+                      checked={fastCargoChecked}
+                      onChange={() => setFastCargoChecked(!fastCargoChecked)}
+                    />
                   </label>
                 </li>
                 <li>
@@ -40,7 +53,12 @@ const CartTotals = () => {
           <tr>
             <th>Total</th>
             <td>
-              <strong id="cart-total">${subTotals.toFixed(2)}</strong>
+              <strong id="cart-total">
+                $
+                {fastCargoChecked === true
+                  ? (subTotals + cargoFee).toFixed(2)
+                  : subTotals.toFixed(2)}
+              </strong>
             </td>
           </tr>
         </tbody>
