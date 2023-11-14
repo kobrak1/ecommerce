e4a362e79028d8ef52a890fca3d4f35614b203d9
kobrak1 <burakkarhan657@gmail.com>
2023-11-14 12:21:54 +0300

    feat: checks if the user already exists with the same email address and hashes the password entered before sending to the database

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const User = require("../models/User.js");
 
-// Kullanıcı Oluşturma (Create - Register)
+// Create-Register new user
 router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    // checkpoint if there is already an existing user with the same email address
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        return res.status(400).json({error:"Email address is already registered."})
+    }
+    // creates a variable to keep the hashed password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = await new User({
       username,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await newUser.save();
