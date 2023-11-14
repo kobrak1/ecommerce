9b5694819af1cc8c16d77baf39f6910ef8afd7e9
kobrak1 <burakkarhan657@gmail.com>
2023-11-14 13:06:58 +0300

    feat: generateRandomAvatar function is added to create random avatar images

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../models/User.js");
 
+// generate random avatar
+const generaterandomAvatar = () => {
+  const randomAvatar = Math.floor(Math.random() * 70 + 1);
+  return `https://i.pravatar.cc/300?img=${randomAvatar}`;
+};
+
 // Create-Register new user
 router.post("/register", async (req, res) => {
   try {
@@ -11,15 +17,20 @@ router.post("/register", async (req, res) => {
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-        return res.status(400).json({error:"Email address is already registered."})
+      return res
+        .status(400)
+        .json({ error: "Email address is already registered." });
     }
     // creates a variable to keep the hashed password
     const hashedPassword = await bcrypt.hash(password, 10);
+    // creates a default avatar
+    const defaultAvatar = generaterandomAvatar();
 
     const newUser = await new User({
       username,
       email,
       password: hashedPassword,
+      avatar: defaultAvatar,
     });
 
     await newUser.save();
