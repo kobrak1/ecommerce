a80c2c40fc2a5284ee54640637a191eb802ac109
kobrak1 <burakkarhan657@gmail.com>
2023-11-14 15:31:53 +0300

    add: user login end point is created

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -42,4 +42,19 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// user login
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ email });
+  // check if there is a user registered with this username or password
+  if (!user) {
+    return res.status(404).json({ error: "Invalid email." });
+  }
+  //   ckeck if the password is true
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ error: "Invalid password." });
+  }
+});
+
 module.exports = router;
