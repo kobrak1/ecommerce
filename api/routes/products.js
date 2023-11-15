cf3ec33bc50f06858ca430289a720cec3b5ed9e6
kobrak1 <burakkarhan657@gmail.com>
2023-11-16 01:54:27 +0300

    feat: Product model is created, user can send new products info to database via products.js

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../models/Product.js");
 
-// Tüm ürünleri getirme (Read- All)
-router.get("/", async (req, res)=> {
-    res.send("Ürünler getirildi!");
-})
+// Add a new category (Create)
+router.post("/", async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
 
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
