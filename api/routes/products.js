f64b89f56c53390b995e9c238fd015b73fc7b675
kobrak1 <burakkarhan657@gmail.com>
2023-11-16 15:55:06 +0300

    feat: user can delete a specific product by id

--- api/routes/products.js
+++ api/routes/products.js
@@ -63,4 +63,23 @@ router.put("/:productId", async (req, res) => {
   }
 });
 
+// Delete a specific product
+router.delete("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ error: "Product not found." });
+    }
+
+    res.status(200).json(deletedProduct);
+    console.log('Data deleted');
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
 module.exports = router;
