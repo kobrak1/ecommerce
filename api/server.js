866460b25a1de1adf5987a0f0d63a7749ee975d8
kobrak1 <burakkarhan657@gmail.com>
2023-12-17 23:01:57 +0300

    new

--- api/server.js
+++ api/server.js
@@ -3,34 +3,19 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const logger = require("morgan");
+const path = require("path"); // Import the 'path' module
 const app = express();
 const mainRoute = require("./routes/index.js");
-const port = 5000;
+const port = process.env.PORT || 5000; // Use the provided PORT or default to 5000
 
 dotenv.config();
-
-// const connect = async () => {
-//   console.log("Connecting");
-//   try {
-//     console.log("Connecting to mongoDB...");
-//     await mongoose.connect(process.env.MONGO_URI);
-//     console.log("Connected to mongoDB");
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//     process.exit(1);
-//   }
-//   console.log("connected");
-// };
 
 const connect = async () => {
   console.log("Connecting");
   try {
-    console.log("Connecting to MongoDB...");
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
+    console.log("Connecting to mongoDB...");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1);
@@ -44,6 +29,17 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api", mainRoute);
+
+// Serve static assets (React build) if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // Serve the React app's HTML file for any other routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, async () => {
   await connect();
