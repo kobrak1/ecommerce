63de7426153f1b33ef9c8da6435ce29814ccdaf5
kobrak1 <burakkarhan657@gmail.com>
2023-12-18 00:02:29 +0300

    new

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Layout } from "./layouts/Layout.jsx";
 import CartProvider from "./context/CartProvider.jsx";
-import {BrowserRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <CartProvider>
       <Layout>
         <App />
       </Layout>
     </CartProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
