bd52eec171b0e5458c5119ad1434f2f29668573f
kobrak1 <burakkarhan657@gmail.com>
2023-12-18 13:21:23 +0300

    new

--- api/server.js
+++ api/server.js
@@ -33,11 +33,11 @@ app.use("/api", mainRoute);
 // Serve static assets (React build) if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("client/dist"));
 
   // Serve the React app's HTML file for any other routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
