6a3657d27de5055466ed8b06e0bfcc974cc12904
kobrak1 <burakkarhan657@gmail.com>
2023-12-18 15:40:12 +0300

    Slider button is now routes to the shop page

--- client/src/components/Sliders/SliderItem.jsx
+++ client/src/components/Sliders/SliderItem.jsx
@@ -1,23 +1,25 @@
-import Link from "antd/es/typography/Link"
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const SliderItem = ({imageSrc}) => {
+const SliderItem = ({ imageSrc }) => {
   return (
     <div className="slider-item fade">
-        <div className="slider-image">
-            <img src={imageSrc} className="img-fluid" alt=""/>
-        </div>
-        <div className="container">
-            <p className="slider-title">SUMMER 2024</p>
-            <h2 className="slider-heading">Save up to 70%</h2>
-            <a href="https://zx-commerce.onrender.com/shop" className="btn btn-lg btn-primary">Explore Now</a>
-        </div>
+      <div className="slider-image">
+        <img src={imageSrc} className="img-fluid" alt="" />
+      </div>
+      <div className="container">
+        <p className="slider-title">SUMMER 2024</p>
+        <h2 className="slider-heading">Save up to 70%</h2>
+        <a href="#" className="btn btn-lg btn-primary">
+          <Link to={"/shop"}>Explore Now</Link>
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 SliderItem.propTypes = {
-  imageSrc : PropTypes.string,
-}
+  imageSrc: PropTypes.string,
+};
 
-export default SliderItem
+export default SliderItem;
