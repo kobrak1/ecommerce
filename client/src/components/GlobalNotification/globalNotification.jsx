c3781be3eabe595f8e2baaa67b8170fd0d250f20
kobrak1 <aburakkarhan@gmail.com>
2025-02-14 22:00:55 +0300

    The component has was created and made dynamic

---
+++ client/src/components/GlobalNotification/globalNotification.jsx
@@ -0,0 +1,17 @@
+import "./globalNotification.css";
+
+function GlobalNotification({status}) {
+  return (
+    <div className={`global-notification ${!status ? "inactive" : ""}`}>
+      <div className="container">
+        <p>
+          SUMMER SALE FOR ALL SWIM SUITS AND FREE EXPRESS INTERNATIONAL DELIVERY
+          - OFF 50%!
+          <a href="shop.html">SHOP NOW</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default GlobalNotification;
